515375bf51291807cb3895ae191fe76fadbd0c02
Saeed Amiri <samiri@uni-bremen.de>
2023-10-01 15:16:26 +0200

    update

--- parallel_update.sh
+++ parallel_update.sh
@@ -117,14 +117,40 @@ function back_up() {
     done
 }
 
+function copy_updated() {
+    local parentDir
 
-export -f log_message prepare_dirs update_files submit_jobs back_up
+    parentDir="$1"Oda
+    pushd "$parentDir" || exit 1
+    updateDir=$(find . -type d -name '*updateAfterDropConstraints' -print -quit)
+    cp $updateDir/topol_updated.top topol.top
+    cp $updateDir/APT_COR_updated.itp APT_COR.itp
+    cp $updateDir/updated_system.gro system.gro
+}
+
+function update_topol() {
+    local parentDir
+
+    parentDir="$1"Oda
+    pushd "$parentDir" || exit 1
+    
+    sed -i '1 ; atopology after updating protonation' topol.top
+
+    # Replace the line
+    sed -i 's|#include "../APT_COR.itp"|#include "./APT_COR.itp"|' topol.top
+
+    # Reove the block of lines
+    sed -i '/; Restraints on NP/,/#endif/d' topol.top
+
+}
+
+export -f log_message prepare_dirs update_files submit_jobs back_up copy_updated
+export -f update_topol
 export REPORT
 
 # Define the list of directories
-dirs=( "10" "15" "20" "200" )
+dirs=( "5" )
 # dirs=( "10" "15" "20" "50" "100" "150" "200" )
-updateSuffix="updateAfterDropConstraints"
 
 case $1 in
     'prepare')
@@ -139,6 +165,13 @@ case $1 in
     'backup')
         parallel back_up ::: "${dirs[@]}"
     ;;
+    'copy_updated')
+        parallel copy_updated ::: "${dirs[@]}"
+    ;;
+    'update_topol')
+        parallel update_topol ::: "${dirs[@]}"
+    ;;
+
     'delete')
         # find . -type d -name "updateSuffix" -exec rm -r {} +
         echo -e "\n\t\tDo it manually@@@!!!!!!\n"
