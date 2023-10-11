df213ec115a020044d8de53eb5018fc952a8e1a5
Saeed Amiri <samiri@uni-bremen.de>
2023-10-11 10:22:16 +0200

    write the hostname

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 echo `date` >> nohup.PID
+echo "hostname: $(hostname)" >> nohup.PID
 echo "submit $0 " >> nohup.PID
 echo "PID of the nohup is:" >> nohup.PID
 nohup bash submit.sh npt.gro > my.log 2>&1 &
