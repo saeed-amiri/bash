f07a5b7dcce0f7b5a38999ba68f5c8aedfee7b65
Saeed Amiri <samiri@uni-bremen.de>
2024-12-13 08:41:26 +0100

    update the compersing sec

--- copy_lammps.sh
+++ copy_lammps.sh
@@ -13,8 +13,15 @@ find "$SRC_DIR" -type d -exec mkdir -p "$DEST_DIR/{}" \;
 # Find and copy *_output.log and run_in.* files
 find "$SRC_DIR" -type f \( -name "*_output_T.log" -o -name "run*in.*" \) -exec rsync -R {} "$DEST_DIR" \;
 
-Find and compress .data files, then copy them
-find "$SRC_DIR" -type f -name "*.data" | while read -r file; do
+
+echo "Copying files from $SRC_DIR to $DEST_DIR"
+# Find and compress .data files, then copy them
+find "$SRC_DIR" -type f -name "system_after_eq3_npt.data" | while read -r file; do
+    echo "Processing $file"
     tar -cJf "$file.tar.xz" -C "$(dirname "$file")" "$(basename "$file")"
+    echo "Compressed $file to $file.tar.xz"
+    echo "Copying $file.tar.xz to $DEST_DIR"
     rsync -R "$file.tar.xz" "$DEST_DIR"
+    echo "Done with $file"
+    echo "############################################"
 done
